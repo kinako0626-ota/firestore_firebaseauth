a0e8b0ebfc931dbc9cc6d77c0a6c60de89ac428d
kinako0626-ota <64303653+kinako0626-ota@users.noreply.github.com>
2020-10-04 09:19:08 +0900

    Delete keys.dart
    設定したはずが何故かコミットされた

--- lib/utilities/keys.dart
+++
@@ -1,7 +0,0 @@
-//ここでAPIを作成
-//Google　Devで発行したAPIを貼り付け
-
-//iOS
-const String IOS_API_KEY = 'AIzaSyAueUGs8t1t8n_ATSTLy6c9EqCr2HOxP6E';
-//Android
-const String ANDROID_API_KEY = 'AIzaSyDMKSKxYYs7elF2mydpYDsQoa1xtS2AMbQ';
